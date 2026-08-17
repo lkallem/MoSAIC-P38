184647dec9a96024db5b50ef079382c9f8d43d1e
Laura Kallem <lkallem@lbl.gov>
2026-08-17 12:29:10 -0700

    Checker-passing long_pkt: Replaced riscv.ld and makefile in c_long_pkt with ones from c_fft_acc, cleaned up perl script, added gen_hex.pl to c_long_pkt.

--- tools/generate/mosaic_2x2_long_pkts.pl
+++ tools/generate/mosaic_2x2_long_pkts.pl
@@ -24,8 +24,8 @@
 
 use lib "$ENV{PWD}";
 use lib "$ENV{PWD}/../picorv_c/c_long_pkt";
-use lib "$ENV{PWD}/../picorv_c/c_long_pkt";
 use gen_mosaic;
+use gen_hex;
 use POSIX;
 
 ###########################################
@@ -38,19 +38,24 @@ use POSIX;
 #- Test case: Modify
 ###########################################
 
+$path = "$ENV{PWD}";
+print "INFO: Current directory: $path\n";
+
+#- Firmware path
+$fw_path = "$path/../picorv_c/c_long_pkt";
+$param{'firmware_path'} = $fw_path;
+
 #- 2x2 Tile array
 $param{'r'} = 2;
 $param{'c'} = 2;
 
 @tile_array = (['pico', 'spad'],
-               ['loop', 'spad']);
+               ['loop', 'pico']);
 
-@pico_program  = ('long_pkt32.hex', '', '', 'recv_pkt32.hex');
-
-# @pico_program  = ('pico_scratchpad32.hex', '', '', 'test_tile_nop.hex');
+@pico_program  = ('long_pkt32_0.hex', '', '', 'recv_pkt32_9.hex');
 
 #- Simulation Time
-$param{'sim_loop'}     = 600;
+$param{'sim_loop'}     = 400;
 
 #- Checkers
 @checkers = ('check_long_pkt.sh');
@@ -58,16 +63,26 @@ $param{'sim_loop'}     = 600;
 #- Running with Vivado - do not turn off Vivado, simulation does not work in Icarus.
 $param{'vivado'} = 1;
 $param{'vivado_project'} = 1;
-#- Running with Vivado
-$param{'vivado'} = 1;
-$param{'vivado_project'} = 1;
 $param{'run_sim'} = 1;
 
+#- C file
+$c_file = 'long_pkt';
+
 #- Generate hex code
-chdir "../picorv_c/c_long_pkt/" or die "Couldn't change to c directory. $!\n";
-`make SRC_FNAME=long_pkt`;
-`make SRC_FNAME=recv_pkt`;
-chdir "../../generate" or die "Couldn't go to generate $!\n";
+chdir $fw_path or die "$!. $fw_path\n";
+%param_h;
+$param_h{'c_code'} = $c_file;
+$param_h{'r'}      = $param{'r'}; 
+$param_h{'c'}      = $param{'c'};             
+$param_h{'keep'}   = 1; 
+$param_h{'clean'}  = 1;
+$param_h{'tile_array'} = \@tile_array;
+gen_code(\%param_h);
+#- C file
+$c_file = 'recv_pkt';
+$param_h{'c_code'} = $c_file;
+gen_code(\%param_h);
+chdir $path or die "$!. $path\n";
 
 ###########################################
 #- Generate: Do not modify  
